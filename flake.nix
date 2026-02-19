d89b719cb26b7d654565033fe1d32a0662ceb9cf
AltF02 <contact@altf2.dev>
2026-02-19 10:32:03 +0100

    feat(flake): introduce direnv flake env
    Signed-off-by: AltF02 <contact@altf2.dev>

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "capacitor";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        nativeBuildInputs = with pkgs; [
+          go_1_26
+          clang
+        ];
+        buildInputs = with pkgs; [
+          gopls
+          delve
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs nativeBuildInputs;
+        };
+      }
+    );
+}
